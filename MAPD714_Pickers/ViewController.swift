5d853eae74f74dff3e388c62e4bd65c0aa254709
Timofei Sopin <timofei.sopin@gmail.com>
2017-10-11 18:45:37 -0400

    Initial Commit

---
+++ MAPD714_Pickers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MAPD714_Pickers
+//
+//  Created by Timofei Sopin on 2017-10-11.
+//  Copyright © 2017 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
